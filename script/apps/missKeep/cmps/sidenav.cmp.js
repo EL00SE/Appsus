e2df80231961cdb4f02f2a1be3ef73fb33666d2f
EL00SE <elooseghantous@gmail.com>
2022-03-06 06:59:36 +0200

    asdasd

--- script/apps/missKeep/cmps/sidenav.cmp.js
+++ script/apps/missKeep/cmps/sidenav.cmp.js
@@ -2,23 +2,22 @@ export default {
     template: `
         <section class="side-nav-container">
             <ul class="nav">
-            <li class="nav-items">
-            <div class="icon nav-text"></div>
-            <button class="fliter-btn txt"></button>
-            </li>
-            <li class="nav-items">
-            <div class="icon nav-todo"></div>
-            <button class="fliter-btn list"></button>
-            </li>
-            <li class="nav-items">
-            <div class="icon nav-img">
-            </div>
-            <button class="fliter-btn img"></button>
-            </li>
-            <li class="nav-items">
-            <div class="icon nav-vid"></div>
-            <button class="fliter-btn vid"></button>
-            </li>
+                <li class="nav-items">
+                    <div class="icon nav-text"></div>
+                    Text
+                </li>
+                <li class="nav-items">
+                    <div class="icon nav-todo"></div>
+                    Todo
+                </li>
+                <li class="nav-items">
+                    <div class="icon nav-img"></div>
+                    Image
+                </li>
+                <li class="nav-items">
+                    <div class="icon nav-vid"></div>
+                    Video
+                </li>
             </ul>
         </section>
     `,
