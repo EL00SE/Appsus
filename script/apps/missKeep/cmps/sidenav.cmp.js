521bf98b30a092df67253d7b0f011c75d4e054f1
EL00SE <elooseghantous@gmail.com>
2022-03-06 06:54:02 +0200

    sidenav

--- script/apps/missKeep/cmps/sidenav.cmp.js
+++ script/apps/missKeep/cmps/sidenav.cmp.js
@@ -2,14 +2,28 @@ export default {
     template: `
         <section class="side-nav-container">
             <ul class="nav">
-            <li></li>
-            <li></li>
+            <li class="nav-items">
+            <div class="icon nav-text"></div>
+            <button class="fliter-btn txt"></button>
+            </li>
+            <li class="nav-items">
+            <div class="icon nav-todo"></div>
+            <button class="fliter-btn list"></button>
+            </li>
+            <li class="nav-items">
+            <div class="icon nav-img">
+            </div>
+            <button class="fliter-btn img"></button>
+            </li>
+            <li class="nav-items">
+            <div class="icon nav-vid"></div>
+            <button class="fliter-btn vid"></button>
+            </li>
             </ul>
         </section>
     `,
     components: {
-        noteList,
-        noteInput
+
     },
     data() {
         return {
